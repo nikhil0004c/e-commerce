c74848e606f0d9b8508efa3e04bdbe64179c8ed7
Nikhil30806 <kn531646@gmail.com>
2026-07-28 09:35:32 +0530

    Add images and JavaScript files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let cartCount = 0;
+
+function addToCart(productName) {
+    cartCount++;
+
+    document.getElementById("cart-count").innerText = cartCount;
+
+    alert(productName + " added to cart!");
+}
