ab8e0f3dbf1e772ec54ed5aa5eed8940455bb49e
Nikhil30806 <kn531646@gmail.com>
2026-07-28 10:24:15 +0530

    Fix category filter

--- js/script.js
+++ js/script.js
@@ -59,3 +59,21 @@ function searchProducts() {
     });
 
 }
+function filterProducts(category) {
+
+    const products = document.querySelectorAll(".product-card");
+
+    products.forEach(product => {
+
+        if (
+            category === "all" ||
+            product.dataset.category === category
+        ) {
+            product.style.display = "block";
+        } else {
+            product.style.display = "none";
+        }
+
+    });
+
+}
